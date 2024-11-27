6636503402fb588bdd3fbe2f7b2a6633ca925ac0
delellisgabriel <delellisgabriel@gmail.com>
2024-11-27 19:15:22 +0100

    feat: adds link markdown content for blog

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,27 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import useModal from "@/hooks/useModal";
 
 export default function Home() {
+  const { openModal, Modal } = useModal();
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        ⚠️ Like in the old days, this website is Under Construction ⚠️
+        ⚠️ Like in the old days, this website is Under Construction ⚠️ Right now
+        I&apos;m just making reusable components with some tests and writting
+        some content for a blog that is gonna live here. A Link to the content
+        is here:{" "}
+        <Link
+          href="https://github.com/delellisgabriel/knowledge-and-experiences"
+          target="_blank"
+        >
+          https://github.com/delellisgabriel/knowledge-and-experiences
+        </Link>
+        <button onClick={() => openModal()}>open modal</button>
+        <Modal>The modal is open 🧙‍♂️ You&apos;re a wizard!</Modal>
       </main>
       <footer className={styles.footer}>
         <a
